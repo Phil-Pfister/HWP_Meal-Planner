3dfabd823c5bc60b9595b21e79be66e26c933fd4
Phil-Pfister <salshouse@gmail.com>
2023-06-01 19:16:25 -0500

    search bar functionality

---
+++ public/js/search.js
@@ -0,0 +1,23 @@
+
+
+const searchFormHandler = async (event) => {
+    event.preventDefault();
+    const QUERY = document.querySelector('#search-bar').value;
+    const response = await fetch(`/search/${QUERY}`, {
+      method: 'GET',
+      
+      
+    });
+    if (response.ok) {
+      document.location.replace(`/search/${QUERY}`);
+    } else {
+      alert('SOMETHING BAD HAPPENED');
+    }
+  };
+
+  
+   
+
+
+
+document.querySelector('.search-button').addEventListener('click', searchFormHandler);
