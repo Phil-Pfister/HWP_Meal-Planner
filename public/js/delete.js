5379f9355c3b8a21e793e0b42103eb129eb20648
Phil-Pfister <salshouse@gmail.com>
2023-06-03 16:30:41 -0500

    debugged delete

--- public/js/delete.js
+++ public/js/delete.js
@@ -1,16 +1,15 @@
 async function deleteFormHandler(event) {
     event.preventDefault();
   
-    const recipe_id = window.location.toString().split('/')[
-      window.location.toString().split('/').length - 1
-    ];
+    const recipe_id = event.target.getAttribute('data-id');
+  
   
     const response = await fetch(`/api/delete/recipe/${recipe_id}`, {
       method: 'DELETE'
     });
   
     if (response.ok) {
-      document.location.replace('/userRecipes');
+      document.location.replace('/recipes');
     } else {
       alert(response.statusText);
     }
