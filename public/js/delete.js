137a516d1563c916e14e05e705fd0ce2f8c6c9c0
Phil-Pfister <salshouse@gmail.com>
2023-06-04 09:55:32 -0500

    debugging delete

--- public/js/delete.js
+++ public/js/delete.js
@@ -16,4 +16,4 @@ async function deleteFormHandler(event) {
   }
   }
   
-  document.querySelector('.rec-card').addEventListener('click', deleteFormHandler);
+  document.querySelector('.rec-card').addEventListener('mousedown', deleteFormHandler);
