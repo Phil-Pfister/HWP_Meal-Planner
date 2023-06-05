1bd065cf6831a4a6e1d13e51299b0d45fd3b11e4
PhilippWinston <philippwinston@gmail.com>
2023-06-04 19:24:34 -0500

    added a catch handling for the alert windows doe login and signup

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,27 +1,36 @@
 // signup form handler and api call 
 
 const signupFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const username = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-  
+  event.preventDefault();
+
+  const username = document.querySelector('#username-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  try {
     if (username && email && password) {
       const response = await fetch('/api/users', {
         method: 'POST',
         body: JSON.stringify({ username, email, password }),
         headers: { 'Content-Type': 'application/json' },
       });
-  
+
       if (response.ok) {
         document.location.replace('/');
       } else {
-        console.log(response);
+        const errorData = await response.json();
+        console.log(errorData);
+        alert(errorData.message || 'An error occurred during signup.');
       }
+    } else {
+      alert('Please fill in all fields.');
     }
-  };
-  
-  document
+  } catch (error) {
+    console.log(JSON.stringify(error));
+    alert('An error occurred. Please try again later.');
+  }
+};
+
+document
   .querySelector('#signup-form')
   .addEventListener('submit', signupFormHandler);
